4a37f82d83cf1bcec0f2d71e6155b9523a14850f
abdelqodousse boustani <bousta_a@etna-alternance.net>
2023-01-13 15:23:36 +0100

    i guess is the commit

--- next-template-main/components/NavBar.tsx
+++ next-template-main/components/NavBar.tsx
@@ -1,7 +1,8 @@
 import styles from '../styles/Home.module.css';
 
 export default function NavBar() {
-  return (<nav className='p-6 bg-[#C2CBD2] shadow md:flex md:items-center md:justify-between '>
+  return (
+  <nav className='p-6 bg-[#C2CBD2] w-screen shadow md:flex md:items-center md:justify-between '>
     <div className='md:flex md:justify-center'>
         <span className='text-[#293845] text-3xl cursor-pointer font-serif'>
     <img className='h-16 inline ' src="https://ythwitum.sirv.com/Images/AB-abstract-logo-design-on-transparent-background-PNG.png"/>
@@ -11,18 +12,15 @@ export default function NavBar() {
     </div>
     <section>
     <ul className='md:flex md:item-center'>
-   
+
         <li className='mx-4'>
-            <a href="A propos" className='text-2xl hover:text-[#343D46]'>A propos</a>
+            <a href="#A-propos" className='text-2xl hover:text-[#343D46]'>A propos</a>
         </li>
         <li className='mx-4'>
-            <a href="#" className='text-2xl hover:text-[#343D46]'>Compétences</a>
-        </li>
-        <li className='mx-4'> 
-            <a href="#" className='text-2xl hover:text-[#343D46]'>Projets</a>
+            <a href="#Compétences" className='text-2xl hover:text-[#343D46]'>Compétences</a>
         </li>
         <li className='mx-4'>
-            <a href="#" className='text-2xl hover:text-[#343D46]'>Contact</a>
+            <a href="#Projets" className='text-2xl hover:text-[#343D46]'>Projets</a>
         </li>
     </ul>
     </section>
---
+++ next-template-main/components/Skills.tsx
@@ -0,0 +1,22 @@
+export default function Skills() {
+  return (
+
+  <><div className="mb-1 mt-4 text-lg font-medium "><img className="h-[4%] w-[4%]" src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"/>  Tailwindcss</div><div className="w-full h-6 mt-2 bg-gray-200 rounded-full">
+    <div className="h-6 bg-blue-300 rounded-full w-[85%]"></div>
+    <div className='text-[#C2CBD2] flex justify-end'>85%</div>
+    </div>
+    <div className="mb-1 mt-4 text-lg font-medium"><img className="h-[4%] w-[4%]" src="https://upload.vectorlogo.zone/logos/nextjs/images/60eff509-53dd-4280-92e7-7318fa02e934.svg"/>Nextjs</div><div className="w-full h-6 mt-2 bg-gray-200 rounded-full">
+    <div className="h-6 bg-blue-300 rounded-full w-[65%]"></div>
+    <div className='text-[#C2CBD2] flex justify-end'>65%</div>
+    </div>
+    <div className="mb-1 mt-4 text-lg font-medium"><img className="h-[4%] w-[4%]" src="https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg"/>Typescript</div><div className="w-full h-6 mt-2 bg-gray-200 rounded-full ">
+    <div className="h-6 bg-blue-300 rounded-full w-[60%]"></div>
+    <div className='text-[#C2CBD2] flex justify-end'>60%</div>
+    </div>
+    <div className="mb-1 mt-4 text-lg font-medium"><img className="h-[4%] w-[4%]" src="https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg"/>HTML</div><div className="w-full h-6 mt-2 bg-gray-200 rounded-full">
+    <div className="h-6 bg-blue-300 rounded-full w-[70%]"></div>
+    <div className='text-[#C2CBD2] flex justify-end'>70%</div>
+    </div>
+  </>
+  );
+}
